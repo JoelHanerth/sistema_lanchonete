82d8d9b8bae4bab409db3489cfccfc467eed6b37
Joel Hanerth <2024122760148@cefetes.br>
2024-10-17 14:46:28 -0300

    criando structs

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
+struct Lanche{
+    char nome[50], ingredientes [300];
+    double preco;
+
+};
+typedef struct Lanche Lanche;
+
+struct Pedido{
+    Lanche tipoLanche;
+    int nPedido, quant;
+    char nomeCliente[100];
+
+};
+
+
+
+
 int main(){
 
     return 0;
