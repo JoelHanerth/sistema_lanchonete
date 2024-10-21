548d8927fbc58f659a9108f9651d8b0e340a9bfa
JoelHanerth <hanerthjoel@gmail.com>
2024-10-21 19:58:53 -0300

    modificacoes

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ typedef struct Pedido Pedido;
 
 struct Pedidos{
     Pedido pedido;
-    int quantPedidos;
+    int quant;
 
 };
 typedef struct Pedidos Pedidos;
@@ -37,10 +37,9 @@ typedef struct Pedidos Pedidos;
 
 // PESQUISAR
 
-int pesquisarLanche (Lanches *vetorLanches, int pesq) {
+int pesquisarLancheID (Lanches vetorLanches[], int pesq) {
     int i;
     for (i = 0; i < vetorLanches->quant; i++) {
-		// printf("endereco lanche %d",vetorLanches[i].lanche.id);
 		
         if (vetorLanches[i].lanche.id == pesq){
             return i;
@@ -49,9 +48,10 @@ int pesquisarLanche (Lanches *vetorLanches, int pesq) {
     return -1;
 }
 
-int pesquisarPedidos (Pedidos *vetorPedidos, int pesq) {
+
+int pesquisarPedidosID (Pedidos vetorPedidos[], int pesq) {
     int i;
-    for (i = 0; i < vetorPedidos->quantPedidos; i++) {
+    for (i = 0; i < vetorPedidos->quant; i++) {
 		
         if (vetorPedidos[i].pedido.id == pesq){
             return i;
@@ -60,6 +60,64 @@ int pesquisarPedidos (Pedidos *vetorPedidos, int pesq) {
     return -1;
 }
 
+int pesquisarPedidosNome (Pedidos vetorPedidos[], char *nome) {
+    int i;
+    for (i = 0; i < vetorPedidos->quant; i++) {
+
+		// Debug: Mostrar o nome que está sendo comparado
+        printf("Comparando: %s com %s\n", vetorPedidos[i].pedido.nomeCliente, nome);
+		// Usa strcmp para comparar o conteúdo das strings
+        if (strcmp(vetorPedidos[i].pedido.nomeCliente, nome) == 0) {
+			printf("aqui");
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+ void imprimirPedido(Pedidos vetorPedidos[], Lanches vetorLanches[], int id){
+	int pesq1, pesq2;
+	printf("\n\n");
+
+	pesq1 = pesquisarPedidosID(vetorPedidos, id);
+	if (pesq1 != -1){
+
+		// pra achar o endereço do lanche correspondente ao pedido
+		pesq2 = pesquisarLancheID(vetorLanches, (vetorPedidos[pesq1].pedido.tipoLanche));
+
+		printf("Número do pedido: %d\n", vetorPedidos[pesq1].pedido.id);
+		printf("ID %d: %s\n", vetorLanches[pesq2].lanche.id ,vetorLanches[pesq2].lanche.nome);
+		printf("Ingredientes: %s\n", vetorLanches[pesq2].lanche.ingredientes);
+		printf("Preço unitário: R$%lf\n", vetorLanches[pesq2].lanche.preco);
+		printf("Quantidade: %d\n", vetorPedidos[pesq1].pedido.quant);
+		printf("Preço final do pedido: R$%lf\n", vetorPedidos[pesq1].pedido.valorPedido);
+		printf("Nome do Cliente: %s\n\n", vetorPedidos[pesq1].pedido.nomeCliente);
+	}
+
+	else{ printf("PEDIDO NÃO ENCOTRADO\n\n"); }
+ }
+
+
+
+void imprimirLanches(Lanches vetorLanches[]){
+	int i;
+    printf("\nLANCHES DISPONÍVEIS:\n");
+    for (i = 0; i < vetorLanches->quant; i++) {
+		printf("ID %d - %s ---- R$%lf\n", vetorLanches[i].lanche.id, vetorLanches[i].lanche.nome, vetorLanches[i].lanche.preco);
+    }
+    printf("\n");
+ }
+
+
+
+ void listar(Pedidos vetorPedidos[], Lanches vetorLanches[]){
+	int i;
+
+    for (i = 0; i < vetorPedidos->quant; i++) {
+		imprimirPedido(vetorPedidos, vetorLanches, vetorPedidos[i].pedido.id);
+    }
+ }
 
 
 double verificarPreco(){
@@ -77,19 +135,30 @@ double verificarPreco(){
 }
 
 
+int obterNumIntPositivo( char *mensagem){
+	int num;
+	do{
+		printf("%s", mensagem);
+    	scanf("%d",&num);
+		if (num<=0){
+			printf("\nNúmero Inválido\n");
+		}
+	} while (num<=0);
+	return num;
+}
 
 // INSERIR DADOS
-void inserirLanche(Lanches *lista_lanches){
+void inserirLanche(Lanches lista_lanches[]){
 	int aux, pesq;
 
 	// numero do pedido
 	do
 	{
-		printf("Informe o id do lanche: ");
+		printf("\nInforme o id do lanche: ");
     	scanf("%d",&aux);
-		pesq = pesquisarLanche(lista_lanches,aux);
+		pesq = pesquisarLancheID(lista_lanches,aux);
 		if (pesq!=-1){
-			printf("\nID JÁ CADASTRADO\n");
+			printf("ID JÁ CADASTRADO\n");
 		}
 	} while (pesq!=-1);
 
@@ -97,25 +166,23 @@ void inserirLanche(Lanches *lista_lanches){
 	lista_lanches[lista_lanches->quant].lanche.id = aux;
 	
 	// nome do lanche
-	printf("\nInforme o nome do lanche: ");
+	printf("Informe o nome do lanche: ");
     scanf(" %49[^\n]s", lista_lanches[lista_lanches->quant].lanche.nome);
 
 	// ingredientes do lanche
-	printf("\nInforme os ingredientes do lanche: ");
+	printf("Informe os ingredientes do lanche: ");
     scanf(" %299[^\n]s", lista_lanches[lista_lanches->quant].lanche.ingredientes);
 
 	// preco do lanche
 	lista_lanches[lista_lanches->quant].lanche.preco = verificarPreco();
 
-	lista_lanches->quant+=1;
+	lista_lanches->quant += 1;
 
 }
 
 
 
-
-
-void inserirPedido(Pedidos *vetorPedidos, Lanches *vetorLanches){
+void inserirPedido(Pedidos vetorPedidos[], Lanches vetorLanches[]){
 	int aux, pesq;
 	double preco;
 
@@ -124,81 +191,59 @@ void inserirPedido(Pedidos *vetorPedidos, Lanches *vetorLanches){
 	{
 		printf("Informe o número do pedido: ");
     	scanf("%d",&aux);
-		pesq = pesquisarPedidos(vetorPedidos,aux);
+		pesq = pesquisarPedidosID(vetorPedidos,aux);
 		if (pesq!=-1){
 			printf("\nPEDIDO JÁ EXISTENTE\n");
 		}
 	} while (pesq!=-1);
 
 	// salva o numero do pedido
-	vetorPedidos[vetorPedidos->quantPedidos].pedido.id=aux;
+	vetorPedidos[vetorPedidos->quant].pedido.id=aux;
 
 
-
-
-	// imprimir menu de lanches disponíveis
 
 	// pega id de lanche
 	do
 	{
+		imprimirLanches(vetorLanches);
+
 		printf("Informe o id do lanche: ");
     	scanf("%d",&aux);
-		pesq = pesquisarLanche(vetorLanches,aux);
+		pesq = pesquisarLancheID(vetorLanches,aux);
 		if (pesq==-1){
 			printf("\nID inexistente\n");
 		}
 	} while (pesq==-1);
 
 	// salva o id
-	vetorPedidos[vetorPedidos->quantPedidos].pedido.tipoLanche=aux;
+	vetorPedidos[vetorPedidos->quant].pedido.tipoLanche=aux;
 
 	// salva o preco do lanche
 	preco = vetorLanches[pesq].lanche.preco;
 
 
 	// quantidade de lanche
-	do{
-		printf("Informe a quantidade de lanche: ");
-    	scanf("%d",&aux);
-		if (aux<=0){
-			printf("\nValor inválido\n");
-		}
-	} while (aux<=0);
+	aux = obterNumIntPositivo("Informe a quantidade de lanche: ");
 
 	// salva a quantidade do pedido
-	vetorPedidos[vetorPedidos->quantPedidos].pedido.quant=aux;
+	vetorPedidos[vetorPedidos->quant].pedido.quant=aux;
 
 	// calcula o valor TOTAL do pedido
-	vetorPedidos[vetorPedidos->quantPedidos].pedido.valorPedido = aux* preco;
+	vetorPedidos[vetorPedidos->quant].pedido.valorPedido = aux* preco;
 
 	// nome do cliente
-	printf("\nInforme o nome do cliente: ");
-    scanf(" %49[^\n]s", vetorPedidos[vetorPedidos->quantPedidos].pedido.nomeCliente);
+	printf("Informe o nome do cliente: ");
+    scanf(" %49[^\n]s", vetorPedidos[vetorPedidos->quant].pedido.nomeCliente);
 
 
-	vetorPedidos->quantPedidos+=1;
+	vetorPedidos->quant+=1;
 
 
 
 }
 
- void imprimirPedido(Pedidos *vetorPedidos, Lanches *vetorLanches){
-	int i, pesq;
-    printf("\n\n");
-    for (i = 0; i < vetorPedidos->quantPedidos; i++) {
-		
-		printf("Número do pedido: %d\n", vetorPedidos[i].pedido.id);
 
-		pesq = pesquisarLanche(vetorLanches, (vetorPedidos[i].pedido.tipoLanche));
-		printf("Produto: %s\n", vetorLanches[pesq].lanche.nome);
-		printf("Ingredientes: %s\n", vetorLanches[pesq].lanche.ingredientes);
-		printf("Quantidade: %d\n", vetorPedidos[i].pedido.quant);
-		printf("Preço final do pedido: %lf\n", vetorPedidos[i].pedido.valorPedido);
-		printf("Nome do Cliente: %s\n\n\n", vetorPedidos[i].pedido.nomeCliente);
-        
-    }
-    printf("\n\n");
- }
+
 
 
 
@@ -232,7 +277,10 @@ int main() {
     SetConsoleOutputCP(65001);
     Pedidos vetorPedidos[100];
 	Lanches vetorLanches[100];
-    int op;
+	vetorLanches->quant = 0;
+	vetorPedidos->quant = 0;
+    int op, num;
+	char nome[50];
 
 
 
@@ -244,20 +292,25 @@ int main() {
 				// SAIR. NÃO PRECISA FAZER NADA
 				break;
 			case 1:
-				inserirLanche(&vetorLanches);
+				inserirLanche(vetorLanches);
 				printf("\n retorno: %d\n",(vetorLanches[0].lanche.id));
 				break;
 			case 2:
-				inserirPedido(&vetorPedidos, &vetorLanches);
+				inserirPedido(vetorPedidos, vetorLanches);
                 printf("\n retorno: %d\n",(vetorPedidos[0].pedido.id));
 				// PESQUISAR POR CODIGO/MATRICULA
 				break;
 			case 3:
-				imprimirPedido(vetorPedidos, &vetorLanches);
-				// PESQUISAR POR CODIGO/MATRICULA
+				num = obterNumIntPositivo("\n\nInforme o número do pedido: ");
+				imprimirPedido(vetorPedidos,vetorLanches,num);
 				break;
 			case 4:
 				// PESQUISAR POR NOME
+				printf("Informe o nome do cliente: ");
+    			scanf(" %49[^\n]s", nome);
+				num = pesquisarPedidosNome(vetorPedidos,nome);
+				num = vetorPedidos[num].pedido.id;
+				imprimirPedido(vetorPedidos,vetorLanches,num);
 				break;
 			case 5:
 				// ATUALIZAR
@@ -269,7 +322,7 @@ int main() {
 				// EXCLUIR
 				break;
 			case 8:
-				// LISTAR
+				listar(vetorPedidos,vetorLanches);
 				break;
 			default:
 				printf ("\n\nOpçãoo inválida!\n\n");
